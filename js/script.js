fab1aa6c162078030b2a3a869afb7a3347631c16
BuildTools <unconfigured@null.spigotmc.org>
2021-10-01 16:59:21 -0400

    Updated

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
+// When the user scrolls down 50px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("to-top").style.display = "block";
+  } else {
+    document.getElementById("to-top").style.display = "none";
+  }
+
+  if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
+    document.getElementById("navtext").innerHTML = "Projects";
+  } else {
+    document.getElementById("navtext").innerHTML = "Welcome";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
 var TxtType = function(el, toRotate, period) {
         this.toRotate = toRotate;
         this.el = el;
