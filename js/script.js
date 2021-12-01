bad2266cddf4cf7aa492501f810d1e0fb56f6e97
BuildTools <unconfigured@null.spigotmc.org>
2021-12-01 15:37:54 -0500

    Added ABOUT section

--- js/script.js
+++ js/script.js
@@ -10,7 +10,10 @@ function scrollFunction() {
 
   navtext = document.getElementById("navtext");
 
-  if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
+  if (document.body.scrollTop > 3860 || document.documentElement.scrollTop > 3860) {
+    navtext.innerHTML = "About";
+    document.getElementById("bg-title").innerHTML = "About";
+  } else if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
     navtext.innerHTML = "Projects";
     document.getElementById("bg-title").innerHTML = "Projects";
   } else {
@@ -22,6 +25,10 @@ function scrollFunction() {
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   window.scrollTo({top: 0, behavior: 'smooth'});
+}
+
+function goToMe() {
+  window.scrollTo({top: 4330, behavior: 'smooth'});
 }
 
 function toggleMenuOn() {
